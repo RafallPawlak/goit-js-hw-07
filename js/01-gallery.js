57de6d42370c2896acbdc63c86891ece585bf003
RafallPawlak <104594858+RafallPawlak@users.noreply.github.com>
2022-09-29 21:02:28 +0200

    Finaly

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+for (const image of galleryItems) {
+    const images =
+    `<div class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+          <img class="gallery__image"
+            src="${image.preview}"
+            data-source="${image.original}"
+            alt="${image.description}"
+         />
+        </a>
+    </div>`;
+    gallery.innerHTML += images;
+};
+
+gallery.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    if (event.target.className !== "gallery__image") {
+        return;
+    };
+    const srcLink = event.target.getAttribute("data-source");
+    const instance = basicLightbox.create(
+        `<img src="${srcLink}" width="800" height="600">`
+    );
+    instance.show();
+
+    window.addEventListener("keydown", (press) => {
+        if (press.key === "Escape") {
+            instance.close();
+        };
+    });
+});
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+for (const image of galleryItems) {
+    const images =
+    `<a class="gallery__item" href="${image.original}">
+        <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+    </a>`;
+    gallery.innerHTML += images;
+};
+
+gallery.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (event.target.className !== "gallery__image") {
+        console.log(event.target.className);
+        return;
+    }
+    let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+});
+    
